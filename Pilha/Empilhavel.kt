8725be6028733a9c23492e0f867739efba8f4b5a
JoaoVitorLemosDEV <161528449+JoaoVitorLemosDEV@users.noreply.github.com>
2024-12-01 12:15:17 -0300

    Add files via upload

---
+++ Pilha/Empilhavel.kt
@@ -0,0 +1,8 @@
+interface Empilhavel {
+    fun empilhar(dado: Any?)
+    fun desempilhar(): Any?
+    fun topo(): Any?
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
